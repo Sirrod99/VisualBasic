b58b44deae544a7d2024b581cd543862b6582d00
Sirrod99 <Sirrod99@gmail.com>
2017-11-01 10:05:11 -0500

    Update

---
+++ DistanceConverter/DistanceConverter/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class KiloConverter
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Inches_Click(sender As Object, e As EventArgs) Handles btnInches.Click
+        lblMessage.Text = "1 Kilometer = 39,370 inches"
+    End Sub
+
+    Private Sub btnFeet_Click(sender As Object, e As EventArgs) Handles btnFeet.Click
+        lblMessage.Text = "1 Kilometer = 3,281 feet"
+    End Sub
+
+    Private Sub btnYards_Click(sender As Object, e As EventArgs) Handles btnYards.Click
+        lblMessage.Text = "1 Kilometer = 1,093.6 yards"
+    End Sub
+
+    Private Sub btnMiles_Click(sender As Object, e As EventArgs) Handles btnMiles.Click
+        lblMessage.Text = "1 Kilometer = 0.6214 miles"
+    End Sub
+
+    Private Sub lblMessage_Click(sender As Object, e As EventArgs) Handles lblMessage.Click
+
+    End Sub
+End Class
---
+++ Hotel/Hotel.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        lblDirection.Visible = True
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
