aca1f9f4f062d3e60147f6bb03bc6652475de786
Sirrod99 <Sirrod99@gmail.com>
2017-11-06 11:26:56 -0600

    Update

---
+++ DateString/DateString/Form1.vb
@@ -0,0 +1,36 @@
+﻿Public Class Form1
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnShowDate_Click(sender As Object, e As EventArgs) Handles btnShowDate.Click
+        lblDateString.Text = txtDayofWeek.Text & ", " & txtMonth.Text &
+             " " & txtDayofMonth.Text & ", " &
+            txtYear.Text
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtDayofWeek.TextChanged
+
+    End Sub
+
+    Private Sub txtMonth_TextChanged(sender As Object, e As EventArgs) Handles txtMonth.TextChanged
+
+    End Sub
+
+    Private Sub txtDayofMonth_TextChanged(sender As Object, e As EventArgs) Handles txtDayofMonth.TextChanged
+
+    End Sub
+
+    Private Sub txtYear_TextChanged(sender As Object, e As EventArgs) Handles txtYear.TextChanged
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtDayofMonth.Clear()
+        txtDayofWeek.Clear()
+        txtMonth.Clear()
+        txtYear.Clear()
+
+    End Sub
+End Class
